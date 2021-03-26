821e991542412cd61758266668066f4cc25b1082
Mike Kellsy <mkellsy@gmail.com>
2021-03-26 00:57:08 -0600

    add honeywell sidecar

---
+++ hoobs-honeywell-sidecar/ui/app.js
@@ -0,0 +1,38 @@
+let interval, dialog;
+
+function update(token) {
+    if (interval) clearInterval(interval);
+    if (dialog) dialog.close();
+
+    window.removeEventListener("message", message);
+
+    interval = null;
+    dialog = null;
+
+    $value = token;
+
+    $close();
+}
+
+function message(event) {
+    if (event.origin === "https://homebridge-honeywell.iot.oz.nu") {
+        try {
+            const data = JSON.parse(event.data);
+
+            if (data.token) update(data.token);
+        } catch (_error) { /* NULL */ }
+    }
+}
+
+window.addEventListener("message", message, false);
+
+const left = (window.screen.width / 2) - (760 / 2);
+const top = ((window.screen.height / 2) - (760 / 2)) / 2;
+
+dialog = window.open(
+    `https://homebridge-honeywell.iot.oz.nu?consumerKey=${encodeURIComponent($config.consumerKey)}&consumerSecret=${encodeURIComponent($config.consumerSecret)}`,
+    "honeywell",
+    `toolbar=no,status=no,menubar=no,resizable=yes,width=760,height=760,top=${top},left=${left}`,
+);
+
+interval = setInterval(() => { dialog.postMessage("origin-check", "https://homebridge-honeywell.iot.oz.nu"); }, 2000);
